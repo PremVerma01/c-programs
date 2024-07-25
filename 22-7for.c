912e96c496770b95fe2f71688d29184f81aa6fa7
Premnarayan Verma <premvrm01@gmail.com>
2024-07-25 12:16:21 +0530

    Add files via upload

---
+++ 22-7for.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int i,j,l=1,k=1,m;
+	for(i=0;i<=4;i++)
+	{
+		
+		for(j=1;j<=i;j++)
+		{
+			printf(" %d",m);
+			m=k+l;
+	        k=l;
+	        l=m;
+				
+		}
+	
+	
+		printf("\n");
+	}
+
+}
